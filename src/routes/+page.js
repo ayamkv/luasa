e65c02062761122ebf49eaa83d9d722707d70138
ayamkv <65036385+ayamkv@users.noreply.github.com>
2023-02-05 01:50:42 +0700

    sorting

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -5,7 +5,7 @@ let publicUrl = import.meta.env.VITE_DIRECTUS_URL;
 export const load = async ({ fetch }) => {
 
     const fetchMsgData = async () => {
-        const msgRes = await fetch(`${publicUrl}/items/msg`)
+        const msgRes = await fetch(`${publicUrl}/items/msg?sort=date_created`)
         const msgData = await msgRes.json()
         return msgData.data
     }
