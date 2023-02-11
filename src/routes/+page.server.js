0f7065faa5301908e4e134c8806ccc4369255db1
ayamkv <realayamkw@gmail.com>
2023-02-11 21:28:43 +0700

    +color

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -5,11 +5,12 @@ import { directus } from '../lib/server/directus';
 
 const msg = directus.items('msg');
 
-async function sendMessage(to, message) {
+async function sendMessage(to, message, color) {
 
     await msg.createOne({
         to: to,
-        messages: message
+        messages: message,
+        color: color
     });
 
   };
@@ -17,18 +18,18 @@ async function sendMessage(to, message) {
 
 export const actions = {
     sendMsg: async ({ request }) => {
-      // TODO log the user in
       const data = await request.formData();
       const msg = String(data.get('message'));
-      const to = String(data.get('to'))
+      const to = String(data.get('to'));
+      const color = String(data.get('color'));
 
       if (!msg) {
         return fail(400, { msg, missing: true })
       }
       
-      sendMessage(to, msg);
+      sendMessage(to, msg, color);
 //      invalidate(() => true);
-      throw redirect(303, '/')
+      // throw redirect(303, '/')
       return {
         success: true
       }
