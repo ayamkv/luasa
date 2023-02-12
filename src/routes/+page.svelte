d663e0620e95457ba33f8c5e332e7cfe00927938
ayamkv <65036385+ayamkv@users.noreply.github.com>
2023-02-12 07:31:08 +0700

    font (#9)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,6 +36,9 @@
 <svelte:head>
   <title>Luasa</title>
     <link rel="icon" href="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Ghost/3D/ghost_3d.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
     <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
 </svelte:head>
 
